2342976830655258a0cf5d3f89f4d4af6cdda0bf
Damien Retzinger <damienwebdev@gmail.com>
2020-01-17 10:57:41 -0500

    feat(rest): add CORS support for Magento 2 REST APIs (#11)

---
+++ Response/HeaderManager.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright © Graycore, LLC. All rights reserved.
+ * See LICENSE.md for details.
+ */
+namespace Graycore\Cors\Response;
+
+use Magento\Framework\App\Response\HeaderProvider\HeaderProviderInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Phrase;
+
+/**
+ * CorsAllowHeadersHeaderProvider is responsible for adding the header
+ * Access-Control-Allow-Headers to a response.
+ * @category  PHP
+ * @package   Graycore_Cors
+ * @author    Graycore <damien@graycore.io>
+ * @copyright Graycore, LLC (https://www.graycore.io/)
+ * @license   MIT https://github.com/graycoreio/magento2-cors/license
+ * @link      https://github.com/graycoreio/magento2-cors
+ */
+class HeaderManager
+{
+    /**
+     * @var HeaderProviderInterface[]
+     */
+    private $headerProviders;
+
+    /**
+     * @param HeaderProviderInterface[] $headerProviderList
+     * @throws LocalizedException In case one of the header providers is invalid
+     */
+    public function __construct($headerProviderList)
+    {
+        foreach ($headerProviderList as $header) {
+            if (!($header instanceof HeaderProviderInterface)) {
+                throw new LocalizedException(new Phrase('The header provider is invalid. Verify and try again.'));
+            }
+        }
+        $this->headerProviders = $headerProviderList;
+    }
+
+    /**
+     * @param \Magento\Framework\App\Response\HttpInterface $subject
+     * @return void
+     * @codeCoverageIgnore
+     */
+    public function beforeSendResponse(\Magento\Framework\App\Response\HttpInterface $subject)
+    {
+        foreach ($this->headerProviders as $provider) {
+            if ($provider->canApply()) {
+                $subject->setHeader($provider->getName(), $provider->getValue());
+            }
+        }
+    }
+}
---
+++ Test/Integration/WebApiResponseTest.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Copyright © Graycore, LLC. All rights reserved.
+ * See LICENSE.md for details.
+ */
+namespace Graycore\Cors\Test\Integration;
+
+use Magento\Framework\App\Response\Http;
+use Magento\TestFramework\TestCase\AbstractController as ControllerTestCase;
+use Zend\Http\Headers;
+
+/**
+ * Tests that the responses to REST API requests
+ * properly respond with the CORS headers in the
+ * default configuration
+ * @category  PHP
+ * @package   Graycore_Cors
+ * @author    Graycore <damien@graycore.io>
+ * @copyright Graycore, LLC (https://www.graycore.io/)
+ * @license   MIT https://github.com/graycoreio/magento2-cors/license
+ * @link      https://github.com/graycoreio/magento2-cors
+ */
+class WebApiResponseTest extends ControllerTestCase
+{
+    const ENDPOINT = '/rest/default/V1/directory/currency';
+
+    public function getResponse() 
+    {
+        return $this->_objectManager->get(\Magento\Framework\Webapi\Rest\Response::class);
+    }
+
+    private function dispatchToRestApi(){
+        ob_start();
+        $this->dispatch(self::ENDPOINT);
+        ob_end_clean();
+    }
+
+    private function dispatchToRestApiWithOrigin(string $origin)
+    {
+        $headers = new Headers();
+        $headers->addHeaderLine('Origin: ' . $origin);
+        $headers->addHeaderLine('Content-Type: application/json');
+        $this->getRequest()->setMethod('GET')->setHeaders($headers);
+        ob_start();
+        $this->dispatch(self::ENDPOINT);
+        ob_end_clean();
+    }
+
+    /**
+     * @group fit
+     * @magentoConfigFixture default/web/graphql/cors_allowed_origins https://www.example.com
+     */
+    public function testTheRestApiResponseContainsCrossOriginHeaders()
+    {
+        $this->dispatchToRestApiWithOrigin("https://www.example.com");
+
+        /** @var Http $response */
+        $response = $this->getResponse();
+
+        $this->assertNotFalse($response->getHeader('Access-Control-Allow-Origin'));
+        $this->assertNotFalse($response->getHeader('Access-Control-Max-Age'));
+    }
+
+    /**
+     * @magentoConfigFixture default/web/graphql/cors_allowed_origins https://www.example.com
+     */
+    public function testItdoesNotAddAnyCrossOriginHeadersToATypicalRequest()
+    {
+        $headers = new Headers();
+        $headers->addHeaderLine('Origin: https://www.example.com');
+        $this->dispatchToRestApi();
+
+        /** @var Http $response */
+        $response = $this->getResponse();
+        $this->assertFalse($response->getHeader('Access-Control-Allow-Origin'));
+    }
+
+    public function testItDoesNotAddAnyCrossOriginHeadersOutOfTheBox()
+    {
+        $this->dispatchToRestApiWithOrigin("https://www.example.com");
+
+        /** @var Http $response */
+        $response = $this->getResponse();
+        $this->assertFalse($response->getHeader('Access-Control-Allow-Origin'));
+        $this->assertFalse($response->getHeader('Access-Control-Max-Age'));
+    }
+
+    public function testTheRestApiWillRespondToAOptionsRequestWithA200Response()
+    {
+        $headers = new Headers();
+        $headers->addHeaderLine('Origin: https://www.example.com');
+        $headers->addHeaderLine('Content-Type: application/json');
+
+        $this->getRequest()->setMethod('OPTIONS')->setHeaders($headers);
+        $this->dispatchToRestApi();
+
+        /** @var Http $response */
+        $response = $this->getResponse();
+        $this->assertSame(200, $response->getHttpResponseCode());
+    }
+
+    /**
+     * @magentoConfigFixture default/web/graphql/cors_allowed_origins https://www.example.com
+     */
+    public function testTheRestApiWillRespondToAOptionsRequestWithCorsHeadersOnTheResponse()
+    {
+        $headers = new Headers();
+        $headers->addHeaderLine('Origin: https://www.example.com');
+        $headers->addHeaderLine('Content-Type: application/json');
+
+        $this->getRequest()->setMethod('OPTIONS')->setHeaders($headers);
+        $this->dispatchToRestApi();
+
+        /** @var Http $response */
+        $response = $this->getResponse();
+        $this->assertNotFalse($response->getHeader('Access-Control-Allow-Origin'));
+        $this->assertNotFalse($response->getHeader('Access-Control-Max-Age'));
+    }
+}
