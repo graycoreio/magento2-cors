3e694a811200ca20886216d46bf53442183abe5a
Damien Retzinger <damienwebdev@gmail.com>
2021-06-01 10:55:38 -0400

    style(rest): line-length

--- Request/RestRequest.php
+++ Request/RestRequest.php
@@ -1,11 +1,13 @@
 <?php
+
 /**
- * @copyright  Copyright 2017 SplashLab
+ * @copyright Copyright 2017 SplashLab
  */
 /**
  * Copyright © Graycore, LLC. All rights reserved.
  * See LICENSE.md for details.
  */
+
 namespace Graycore\Cors\Request;
 
 use Magento\Framework\Webapi\Request;
@@ -16,14 +18,19 @@ class RestRequest
     /**
      * Triggers before original dispatch
      *
-     * @param Request $subject
+     * @param  Request $subject
      * @return void
      * @throws InputException
      */
     public function aroundGetHttpMethod(
         Request $subject
     ) {
-        if (!$subject->isGet() && !$subject->isPost() && !$subject->isPut() && !$subject->isDelete() && !$subject->isOptions()) {
+        if (!$subject->isGet()
+            && !$subject->isPost()
+            && !$subject->isPut()
+            && !$subject->isDelete()
+            && !$subject->isOptions()
+        ) {
             throw new InputException(__('Request method is invalid.'));
         }
         return $subject->getMethod();
