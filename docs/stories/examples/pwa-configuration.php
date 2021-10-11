c4bb3edce0498a8e76aa8ffa8f3ff7684ab8c6d4
Kristof, Fooman <kristof@fooman.co.nz>
2021-10-12 11:48:36 +1300

    docs: update PWA-Studio example configuration (#59)
    This is from our config we use in production on our site. I am not 100% sure if DNT and TE are a must but I have seen Firefox send those headers.

--- docs/stories/examples/pwa-configuration.php
+++ docs/stories/examples/pwa-configuration.php
@@ -6,14 +6,15 @@ return [
             'web' => [
                 'graphql' => [
                     'cors_max_age' => 86400,
+                    'cors_allow_credentials' => 1,
                     'cors_allowed_methods' => 'POST, OPTIONS, GET',
-                    'cors_allowed_headers' => 'Content-Currency, Store, X-Magento-Cache-Id',
+                    'cors_allowed_headers' => 'Content-Currency, Store, X-Magento-Cache-Id, X-Captcha, Content-Type, Authorization, DNT, TE',
                     // Angular
                     // 'cors_allowed_origins' => 'http://localhost:4200, https://localhost:4200',
                     // Angular + Universal
                     // 'cors_allowed_origins' => 'http://localhost:4200, https://localhost:4200, http://localhost:4000, https://localhost:4000',
                     // Express
-                    // 'cors_allowed_origins' => 'http://localhost:3000, https://localhost:3000', 
+                    // 'cors_allowed_origins' => 'https://frontend.example.com, http://localhost:3000, https://localhost:3000', 
                     // PWA Studio (PWA Studio Generates a random port, so we can't provide a configuration)
                 ]
             ]
