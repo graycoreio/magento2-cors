ef442ad6b30253d24db984f58e061d7154648324
Damien Retzinger <damienwebdev@gmail.com>
2025-04-15 15:50:58 -0400

    test: replace Zend w/ Laminas\Http\Headers (#90)

--- Test/Integration/Preflight/WebApiResponseTest.php
+++ Test/Integration/Preflight/WebApiResponseTest.php
@@ -9,7 +9,7 @@ namespace Graycore\Cors\Test\Integration\Preflight;
 
 use Magento\Framework\App\Response\Http;
 use Magento\TestFramework\TestCase\AbstractController as ControllerTestCase;
-use Zend\Http\Headers;
+use Laminas\Http\Headers;
 
 /**
  * Tests that the responses to REST API requests
